494548b9b3cb6f5a6cd67f11c8a9e71d889fb926
JenniferFulton <jennifer.reeves8@gmail.com>
2022-05-15 12:49:14 -0400

    able to proccess URL request in python file

--- recipes.py
+++ recipes.py
@@ -1,9 +1,13 @@
+import requests as req
+
 #create a function that will search spoonacular recipes and print results
 def recipes(food):
-    print(f"Recipes for {food}")
+    print(f"Recipes for {food}:")
+    resp = req.get("https://api.spoonacular.com/recipes/complexSearch?query=banana&apiKey=2df2ffb1f528442cae5578169a24ec0e")
+    print(resp.text)
 
 #get user input for recipe search
-food = input('Food in Recipe: ')
+food = input('Search Recipes: ')
 
 #call recipe function to get recipes user searched for
 recipes(food)
