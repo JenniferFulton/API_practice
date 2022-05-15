f414d91c28cbf07020198b89fdbe5cb5faca47ef
JenniferFulton <jennifer.reeves8@gmail.com>
2022-05-15 11:41:13 -0400

    function set up that will allow user to search for a recipe

---
+++ recipes.py
@@ -0,0 +1,10 @@
+#create a function that will search spoonacular recipes and print results
+def recipes(food):
+    print(f"Recipes for {food}")
+
+#get user input for recipe search
+food = input('Food in Recipe: ')
+
+#call recipe function to get recipes user searched for
+recipes(food)
+
